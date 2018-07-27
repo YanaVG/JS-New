c8fa3efae567fa8314898abf5f1d5b055d2a1753
YanaVG <yanavg18@gmail.com>
2018-07-27 17:51:49 +0300

    correct function on button start

--- Module9/js/main.js
+++ Module9/js/main.js
@@ -1,36 +1,3 @@
-// Создайте скрипт приложения-секундомера.
-
-// Изначально в HTML есть разметка:
-
-// 00:00.0
-
-// Start Lap Reset
-// Добавьте следующий функционал:
-
-// При нажатии на кнопку button.js-start, запускается таймер, который считает время со старта и до текущего момента времени, обновляя содержимое элемента p.js-time новым значение времени в формате xx:xx.x (минуты:секунды.сотни_миллисекунд).
-
-// Когда секундомер запущен, текст кнопки button.js-start меняется на 'Pause', а функционал при клике превращается в оставновку секундомера без сброса значений времени.
-
-// Если секундомер находится в состоянии паузы, текст на кнопке button.js-start меняется на 'Continue'. При следующем клике в нее, таймер продолжает отсчет времени, как будто паузы небыло, а текст меняется на 'Pause'. 
-//То есть если во время нажатия 'Pause' прошло 6 секунд со старта, при нажатии 'Continue' 10 секунд спустя, секундомер продолжит отсчет времени с 6 секунд и дальше, а не с 16.
-
-// Если секундомер находится в активном состоянии или в состоянии паузы, кнопка button.js-reset должна быть активна (на нее можно кликнуть), в противном случае disabled. Функционал при клике - остановка таймера и сброс всех полей в исходное состояние.
-
-// Функционал кнопки button.js-take-lap при клике - сохранение текущего времени секундомера в массив и добавление в ul.js-laps нового li с сохраненным временем в формате xx:xx.x
-
-// ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
-
-// Выполните домашнее задание используя класс с полями и методами.
-
-// На вход класс Stopwatch принимает только ссылку на DOM-узел в котором будет динамически создана вся разметка для секундомера.
-
-// Должна быть возможность создать сколько угодно экземпляров секундоментов на странице и все они будут работать независимо.
-
-// К примеру:
-
-// new Stopwatch(parentA); new Stopwatch(parentB); new Stopwatch(parentC);
-
-// Где parent* это существующий DOM-узел.
 "use strict"
 
 const stopwatch = document.querySelector('.stopwatch');
@@ -65,7 +32,7 @@ constructor ({
     this.startTime = 0,
     this.deltaTime = null,
     this.timerId = null,
-    this.pauseTime = null,
+    // this.pauseTime = null,
     this.isActive = false,
     this.laps =[]
     this.onTick = onTick,
@@ -74,15 +41,14 @@ constructor ({
 }
     start() {
         if(!this.isActive) {
-            console.log('start');
+            // console.log('start');
             this.isActive = true;
             this.startTime = new Date();
-            this.pauseTime = 0;
-            this.startTime = new Date() - this.pauseTime;
+            this.startTime = new Date() - this.deltaTime;
 
             this.timerId = setInterval(() => {
-                let currantTime = new Date();
-                this.deltaTime = currantTime - this.startTime;
+                // let currantTime = new Date();
+                this.deltaTime = new Date() - this.startTime;
                 let time = new Date(this.deltaTime);
                 this.onTick( time )
             }, 100)
@@ -90,7 +56,6 @@ constructor ({
         } else {
             this.isActive = false;
             clearInterval(this.timerId);
-            this.pauseTime = this.deltaTime;
             startBtn.textContent = 'Continue';
         } 
     }
@@ -99,7 +64,6 @@ constructor ({
         this.isActive = false;
         this.startTime = 0;
         this.timerId = null;
-        this.pauseTime = null;
         this.deltaTime = null;
         this.onTick(this.startTime);
         startBtn.textContent = 'Start';
@@ -109,10 +73,8 @@ constructor ({
     lap() {
         const lapTime = this.onPush(this.deltaTime);
         this.laps.push(lapTime);
-        console.log('this.laps', this.laps);
         this.onLap(lapTime);
     }
-
 };
 
 const timer = new Timer({
